77ac284c48455187d30e90becfdc0453cfd47915
rafaelacardoso928 <cardosorafaela.2802@gmail.com>
2025-03-10 17:53:01 -0300

    Add files via upload

---
+++ AnimaisMarinhos/src/animaismarinhos/AnimaisMarinhos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animaismarinhos;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class AnimaisMarinhos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
